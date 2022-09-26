67c34246a6e3e3cf70ac0fbde9148ac1b0863ae1
Trevor Smith <trevorsmithbanjo@gmail.com>
2022-09-26 12:26:40 -0500

    Creates a function for adding shoppers

--- Rescue Selector/HTML - REscue Selector/myScript.js
+++ Rescue Selector/HTML - REscue Selector/myScript.js
@@ -1,308 +1,121 @@
 'use strict';
 
-
-
 ///////////////////TMS///////////////////////
-const alejandroL = "Alex L";
-const alejandroV = "Alejandro V";
-const alexR = "Ritter";
-const anaG = "Ana";
-const audreyH = "Audrey";
-const brianH = "Brian";
-const chandaO = "Chanda";
-const christineD = "Chris D";
-const chrisP = "Chris P";
-const clarissaS = "Clarissa";
-const deniellW = "Deniell";
-const emilyW = "Emily";
-const ericA = "Eric";
-const gabriellaL = "Gabriella";
-const guilianaA = "Guliana";
-const joetteV = "Joette";
-const joseM = "Jose";
-const joshS = "Josh";
-const kylahC = "Kylah";
-const lisetteB = "Lisette";
-const loriH = "Lori";
-const marisaG = "Marisa";
-const mayaS = "Maya";
-const monicaB = "Monica B";
-const monicaR = "Monica R";
-const naiyaR = "Naiya";
-const nickE = "Nick"
-const sherrieB = "Sherrie";
-const sienaC = "Siena";
-const sofiaP = "Sofia";
-const tavianG = "Tavian";
-const tiffL = "Tiff";
-const zenithC = "Zenith";
-const fabiolaP = "Fabiola";
-const nancyG = "Nancy";
+const alejandroL = 'Alex L';
+const alejandroV = 'Alejandro V';
+const alexR = 'Ritter';
+const anaG = 'Ana';
+const audreyH = 'Audrey';
+const brianH = 'Brian';
+const chandaO = 'Chanda';
+const christineD = 'Chris D';
+const chrisP = 'Chris P';
+const clarissaS = 'Clarissa';
+const deniellW = 'Deniell';
+const emilyW = 'Emily';
+const ericA = 'Eric';
+const gabriellaL = 'Gabriella';
+const guilianaA = 'Guliana';
+const joetteV = 'Joette';
+const joseM = 'Jose';
+const joshS = 'Josh';
+const kylahC = 'Kylah';
+const lisetteB = 'Lisette';
+const loriH = 'Lori';
+const marisaG = 'Marisa';
+const mayaS = 'Maya';
+const monicaB = 'Monica B';
+const monicaR = 'Monica R';
+const naiyaR = 'Naiya';
+const nickE = 'Nick';
+const sherrieB = 'Sherrie';
+const sienaC = 'Siena';
+const sofiaP = 'Sofia';
+const tavianG = 'Tavian';
+const tiffL = 'Tiff';
+const zenithC = 'Zenith';
+const fabiolaP = 'Fabiola';
+const nancyG = 'Nancy';
 
 ///////////BASE TM ARRAY////////////////
-let tmList = [chrisP, joshS, guilianaA, gabriellaL, naiyaR, alejandroL, alejandroV, alexR, anaG, audreyH, brianH, chandaO, christineD, clarissaS, deniellW, emilyW, ericA, joetteV, joseM, kylahC, lisetteB, loriH, marisaG, mayaS, monicaB, monicaR, nickE, sherrieB, sienaC, sofiaP, tavianG, tiffL, zenithC, fabiolaP, nancyG];
-
-
-
+let tmList = [
+  chrisP,
+  joshS,
+  guilianaA,
+  gabriellaL,
+  naiyaR,
+  alejandroL,
+  alejandroV,
+  alexR,
+  anaG,
+  audreyH,
+  brianH,
+  chandaO,
+  christineD,
+  clarissaS,
+  deniellW,
+  emilyW,
+  ericA,
+  joetteV,
+  joseM,
+  kylahC,
+  lisetteB,
+  loriH,
+  marisaG,
+  mayaS,
+  monicaB,
+  monicaR,
+  nickE,
+  sherrieB,
+  sienaC,
+  sofiaP,
+  tavianG,
+  tiffL,
+  zenithC,
+  fabiolaP,
+  nancyG,
+];
 
 ////////////SELECTING TMS//////////////////
 
-
-
 /////////////////ARRAY///////////////////
 
-  let tmList2 = [];
-  let tmList3 = [];
-  let tmList4 = [];
+let tmList2 = [];
+let tmList3 = [];
+let tmList4 = [];
 
 ///////////////////////TM BUTTONS//////////////////
 
-///////////ALEJANDRO L//////////////
-function alPush(list){
-const addAJ = tmList2.push(alejandroL);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(alejandroL));
-document.querySelector('ul').appendChild(node);
-};
-///////////ALEJANDRO V//////////////
-function avPush(list){
-const addAV = tmList2.push(alejandroV);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(alejandroV));
-document.querySelector('ul').appendChild(node);
-};
-///////////RITTER//////////////
-function arPush(list){
-const addAV = tmList2.push(alexR);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(alexR));
-document.querySelector('ul').appendChild(node);
-};
-///////////ANA//////////////
-function agPush(list){
-const addAV = tmList2.push(anaG);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(anaG));
-document.querySelector('ul').appendChild(node);
-};
-///////////AUDREY//////////////
-function ahPush(list){
-const addAV = tmList2.push(audreyH);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(audreyH));
-document.querySelector('ul').appendChild(node);
-};
-///////////BRIAN/////////////
-function bhPush(list){
-const addAJ = tmList2.push(brianH);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(brianH));
-document.querySelector('ul').appendChild(node);
-};
-//////////CHANDA//////////////
-function coPush(list){
-const addAJ = tmList2.push(chandaO);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(chandaO));
-document.querySelector('ul').appendChild(node);
-};
-/////////CHRISTINE///////////
-function cdPush(list){
-const addAJ = tmList2.push(christineD);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(christineD));
-document.querySelector('ul').appendChild(node);
-};
-//////////CHRIS/////////////
-function cpPush(list){
-const addAJ = tmList2.push(chrisP);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(chrisP));
-document.querySelector('ul').appendChild(node);
-};
-////////////CLARISSA/////////
-function csPush(list){
-const addAJ = tmList2.push(clarissaS);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(clarissaS));
-document.querySelector('ul').appendChild(node);
-};
-////////////DENIELL//////////
-function dwPush(list){
-const addAJ = tmList2.push(deniellW);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(deniellW));
-document.querySelector('ul').appendChild(node);
-};
-///////////EMILY////////////
-function ewPush(list){
-const addAJ = tmList2.push(emilyW);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(emilyW));
-document.querySelector('ul').appendChild(node);
-};
-///////////ERIC////////////
-function eaPush(list){
-const addAJ = tmList2.push(ericA);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(ericA));
-document.querySelector('ul').appendChild(node);
-};
-///////////GABRIELLA////////////
-function glPush(list){
-const addAJ = tmList2.push(gabriellaL);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(gabriellaL));
-document.querySelector('ul').appendChild(node);
-};
-///////////GULIANA////////////
-function gaPush(list){
-const addAJ = tmList2.push(guilianaA);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(guilianaA));
-document.querySelector('ul').appendChild(node);
-};
-///////////JOETTE////////////
-function jvPush(list){
-const addAJ = tmList2.push(joetteV);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(joetteV));
-document.querySelector('ul').appendChild(node);
-};
-///////////JOSE////////////
-function jmPush(list){
-const addAJ = tmList2.push(joseM);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(joseM));
-document.querySelector('ul').appendChild(node);
-};
-///////////JOSH////////////
-function jsPush(list){
-const addAJ = tmList2.push(joshS);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(joshS));
-document.querySelector('ul').appendChild(node);
-};
-///////////KYLAH////////////
-function kcPush(list){
-const addAJ = tmList2.push(kylahC);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(kylahC));
-document.querySelector('ul').appendChild(node);
-};
-///////////LISETTE////////////
-function lbPush(list){
-const addAJ = tmList2.push(lisetteB);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(lisetteB));
-document.querySelector('ul').appendChild(node);
-};
-///////////LORI////////////
-function lhPush(list){
-const addAJ = tmList2.push(loriH);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(loriH));
-document.querySelector('ul').appendChild(node);
-};
-///////////MARISA////////////
-function mgPush(list){
-const addAJ = tmList2.push(marisaG);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(marisaG));
-document.querySelector('ul').appendChild(node);
-};
-///////////MAYA////////////
-function msPush(list){
-const addAJ = tmList2.push(mayaS);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(mayaS));
-document.querySelector('ul').appendChild(node);
-};
-///////////MONICA B////////////
-function mbPush(list){
-const addAJ = tmList2.push(monicaB);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(monicaB));
-document.querySelector('ul').appendChild(node);
-};
-///////////MONICA R////////////
-function mrPush(list){
-const addAJ = tmList2.push(monicaR);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(monicaR));
-document.querySelector('ul').appendChild(node);
-};
-///////////NAIYA////////////
-function nrPush(list){
-const addAJ = tmList2.push(naiyaR);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(naiyaR));
-document.querySelector('ul').appendChild(node);
-};
-///////////NICK////////////
-function nePush(list){
-const addAJ = tmList2.push(nickE);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(nickE));
-document.querySelector('ul').appendChild(node);
-};
-///////////SHERRIE////////////
-function sbPush(list){
-const addAJ = tmList2.push(sherrieB);
-var node = document.createElement('li');
-node.appendChild(document.createTextNode(sherrieB));
-document.querySelector('ul').appendChild(node);
-};
-///////////SIENA////////////
-function scPush(list){
-  const addAJ = tmList2.push(sienaC);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(sienaC));
-  document.querySelector('ul').appendChild(node);
-};
-///////////SOFIA////////////
-function spPush(list){
-  const addAJ = tmList2.push(sofiaP);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(sofiaP));
-  document.querySelector('ul').appendChild(node);
-};
-///////////TAVIAN////////////
-function tgPush(list){
-  const addAJ = tmList2.push(tavianG);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(tavianG));
-  document.querySelector('ul').appendChild(node);
-};
-///////////TIFF////////////
-function tlPush(list){
-  const addAJ = tmList2.push(tiffL);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(tiffL));
-  document.querySelector('ul').appendChild(node);
-};
-///////////ZENITH////////////
-function zcPush(list){
-  const addAJ = tmList2.push(zenithC);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(zenithC));
-  document.querySelector('ul').appendChild(node);
-};
-///////////FABIOLA////////////
-function fbPush(list){
-  const addAJ = tmList2.push(fabiolaP);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(fabiolaP));
-  document.querySelector('ul').appendChild(node);
-};
-///////////NANCY////////////
-function ngPush(list){
-  const addAJ = tmList2.push(nancyG);
-  var node = document.createElement('li');
-  node.appendChild(document.createTextNode(nancyG));
-  document.querySelector('ul').appendChild(node);
-};
+// select the container div for the buttons
+const shopperDiv = document.querySelector('.shoppers');
 
+// loop through tmList array with the forEach method
+// i named the value of each name in the array 'shopper' by passing that as the argument in the forEach method
+// this name could be anything you want
+tmList.forEach((shopper) => {
+  // for each shopper in the array we create a button element where the innerText is equal to the value of the shopper in the array
+  const shopperButton = document.createElement('button');
+  shopperButton.innerText = shopper;
+
+  // we append each button to the container div
+  shopperDiv.appendChild(shopperButton);
+
+  // then we add an event listening for the click event and pass the value of the event.target.innerText as the argument for the addShopper function
+  shopperButton.addEventListener('click', (event) => {
+    addShopper(event.target.innerText);
+  });
+});
+
+// function to add a shopper to the tmList2 as well as creating the element to appened to the selected shoppers container
+// this removes the need to have a function for each shopper in the array and instead we just need this one function
+function addShopper(shopper) {
+  console.log(shopper);
+  tmList2.push(shopper);
+
+  let node = document.createElement('li');
+  node.appendChild(document.createTextNode(shopper));
+  document.querySelector('ul').appendChild(node);
+}
 
 ////////////////GENERATE SHOPPER BUTTON////////////////////
 
@@ -311,56 +124,61 @@ const buttonRescueGenerate = document.querySelector('.button-rescue-generate');
 let selectedRescueTM = document.querySelector('.selected1');
 let selectedDropoffTM = document.querySelector('.selected2');
 const showImage = () => {
-    document.getElementById("fireworks").style.display ='block';
+  document.getElementById('fireworks').style.display = 'block';
 };
 const showImage2 = () => {
-    document.getElementById("fire").style.display ='block';
+  document.getElementById('fire').style.display = 'block';
 };
-buttonRescueGenerate.addEventListener('click', function (){
-  function check (list) {if (tmList.length > 13) {
-  tmList.length = 13;
-  Array.prototype.push.apply(tmList3, tmList);
-  tmList4 = tmList2.filter(element => tmList3.includes(element));
-  showImage();
-}};
-check(tmList2);
+buttonRescueGenerate.addEventListener('click', function () {
+  function check(list) {
+    if (tmList.length > 13) {
+      tmList.length = 13;
+      Array.prototype.push.apply(tmList3, tmList);
+      tmList4 = tmList2.filter((element) => tmList3.includes(element));
+      showImage();
+    }
+  }
+  check(tmList2);
   if (tmList4.length > 0) {
-  let random = Math.floor(Math.random() * tmList4.length);
-  selectedRescueTM.textContent = (tmList4[random]);
-  function remove(list) {
-    const index = tmList4.indexOf(tmList4[random]);
-  if (index > -1) {
-    tmList4.splice(index, 1);
-  }};
-  function remove2(list) {
-    const index = tmList4.indexOf(tmList2[random]);
-  if (index > -1) {
-    tmList2.splice(index, 1);
-  }};
-  remove2(tmList2[random]);
-  remove(tmList4[random]);
-}else {
-  let random = Math.floor(Math.random() * tmList2.length);
-  selectedRescueTM.textContent = (tmList2[random]);
-  function remove(list) {
-    const index = tmList2.indexOf(tmList2[random]);
-  if (index > -1) {
-    tmList2.splice(index, 1);
-  }};
-  remove(tmList2[random]);
-}
-// dropoff();
-//  function dropoff(){
-//   showImage2();
-//   if (tmList4.length > 0) {
-//   let random = Math.floor(Math.random() * tmList4.length);
-//   selectedDropoffTM.textContent = (tmList4[random]);
-// }else {
-//     let random = Math.floor(Math.random() * tmList2.length);
-//     selectedDropoffTM.textContent = (tmList2[random]);
-//     console.log(tmList2);
-// }
-// }
+    let random = Math.floor(Math.random() * tmList4.length);
+    selectedRescueTM.textContent = tmList4[random];
+    function remove(list) {
+      const index = tmList4.indexOf(tmList4[random]);
+      if (index > -1) {
+        tmList4.splice(index, 1);
+      }
+    }
+    function remove2(list) {
+      const index = tmList4.indexOf(tmList2[random]);
+      if (index > -1) {
+        tmList2.splice(index, 1);
+      }
+    }
+    remove2(tmList2[random]);
+    remove(tmList4[random]);
+  } else {
+    let random = Math.floor(Math.random() * tmList2.length);
+    selectedRescueTM.textContent = tmList2[random];
+    function remove(list) {
+      const index = tmList2.indexOf(tmList2[random]);
+      if (index > -1) {
+        tmList2.splice(index, 1);
+      }
+    }
+    remove(tmList2[random]);
+  }
+  // dropoff();
+  //  function dropoff(){
+  //   showImage2();
+  //   if (tmList4.length > 0) {
+  //   let random = Math.floor(Math.random() * tmList4.length);
+  //   selectedDropoffTM.textContent = (tmList4[random]);
+  // }else {
+  //     let random = Math.floor(Math.random() * tmList2.length);
+  //     selectedDropoffTM.textContent = (tmList2[random]);
+  //     console.log(tmList2);
+  // }
+  // }
 });
 /////////////////MOVE SELECTION TO THE BOTTOM////////////////
 
